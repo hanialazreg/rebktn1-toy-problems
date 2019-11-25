71c4ed4793ea79ea55e534c1e042835478fb8d84
hanialazreg <lazreghania@gmail.com>
2019-11-25 09:17:17 +0100

    first

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,26 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  // i will split the string, then for each character get the number of repeatitions
+  // represented on key/value like A:3 which mean that A repeated 3 times.
+  // than for all values equal to 1 return the minimum index of them.
+ 
+  var strChar = string.split();
+  var strNumChar = [];
+  var nbRepeatChar = 1;
+  var obj={};
+  var arrIndex = [];
+  	for (var i = 0; i < strChar.length; i++) {
+  	 	for (var j = 1; i < strChar.length; j++) {
+  	 		if(strChar[i] === strChar[j]){
+
+  	 			nbRepeatChar ++;
+  	 			obj.strChar[i].value = nbRepeatChar;
+  	 			arrIndex.push()
+
+
+  	 		}
+  	 	}
+  	 } 
+  	 
 };
