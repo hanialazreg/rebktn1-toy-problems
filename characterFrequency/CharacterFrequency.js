bcfa818fad8e7e82e658e6fa3b5941fa4f10188b
hanialazreg <lazreghania@gmail.com>
2019-12-06 09:58:24 +0100

    presq

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,43 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+function characterFrequency(string){
+	//convert to ascii code then sort the array and count charfrequency.
+	var arr = string.split('');
+	var str = "";
+	var fstr = "";
+	var arr2 = [];
+	var cnt = 1;
+	var farr = [];
+	for (var i = 0; i < arr.length; i++) {
+		str+= arr[i];
+	}
+	for (var i = 0; i < str.length; i++) {
+		fstr+= " " + str.charCodeAt(i);
+	}
+	arr2 = fstr.split(" ");
+	arr2.sort();
+
+	for (var i = arr2.length; i > 0; i--) {
+		cnt =count(arr2,arr2[i]);
+	
+		farr.push(String.fromCharCode(arr2[i]));
+		farr.push(cnt);
+		
+	}
+	return farr;
+	
+}
+
+function count(array,char){
+	var count = 0;
+	for (var i = 0; i < array.length; i++) {
+		if(array[i] === char){
+			count ++;
+		}
+	}
+	return count ;
+}
+
+
--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -11,3 +11,54 @@
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
 // Provided string will always be lower case, won't be empty and will have no special characters.
+
+function PlayingWithString(string){
+	var arr = string.split('');
+	var newch ;
+	var movedStr = "";
+	for (var i = 0; i < string.length; i++) {
+		newch = charMove(string[i]);
+		if(newch === "c" || newch === "o" || newch === "d" || newch === "e"){
+			newch = string[i];
+		}
+		movedStr = movedStr + newch ;
+	}
+	
+	return movedStr;
+
+}
+
+
+// a function that moves characters 
+function charMove(char){
+	var newChar ;
+	if(char === "i" || char === "u" || char === "y"){
+		newChar = String.fromCharCode(char.charCodeAt(0) - 5) ;
+		return newChar;
+	}
+	
+	if(char === "c" || char === "o"){
+		newChar = String.fromCharCode(char.charCodeAt(0) - 1) ;
+		return newChar;
+	}
+
+	if(char === "d"){
+		newChar = String.fromCharCode(char.charCodeAt(0) - 3) ;
+		return newChar;
+	}
+	if(char === "e"){
+		newChar = String.fromCharCode(char.charCodeAt(0) - 4) ;
+		return newChar;
+	}
+	if(char === "a"){
+		newChar = "v" ;
+		return newChar;
+	}
+	if(char === "z"){
+		return "e"
+	}
+	if((char.charCodeAt(0) - 122 ) < 10){
+	return newChar = String.fromCharCode(97 + (char.charCodeAt(0) - 122 ));
+	}
+
+}
