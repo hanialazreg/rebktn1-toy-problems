7a05c19a97f3f1c9b4780af25533945aa7e50261
hanialazreg <lazreghania@gmail.com>
2019-12-11 09:57:38 +0100

    first

--- mergeTwoBinaryTrees/merge.js
+++ mergeTwoBinaryTrees/merge.js
@@ -1,7 +1,10 @@
 /*
-Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+Given two binary trees and imagine that when you put one of them to cover the other,
+some nodes of the two trees are overlapped while the others are not.
 
-You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+You need to merge them into a new binary tree. The merge rule is that if two nodes
+overlap, then sum node values up as the new value of the merged node. Otherwise,
+the NOT null node will be used as the node of new tree.
 
 Example 1:
 
@@ -36,4 +39,49 @@ Note: The merging process must start from the root nodes of both trees.
  * @param {TreeNode} t2
  * @return {TreeNode}
  */
-var mergeTrees = function(t1, t2) {};
+
+ //  first
+var mergeTrees = function(t1, t2) {
+// create a new tree
+// start from the root node do the summation and add it to the new tree then 
+// every time call the left and right child if two exist do the summation 
+// else add the not null node to the new tree.
+var newTree = new TreeNode()
+	if(t1.val && t2.val){
+		newTree.val = t1.val + t2.val;
+	}
+	if(t1.left && t2.left){
+		newTree.left = mergeTrees(t1.left, t2.left);
+	}
+	if(t1.right && t2.right){
+		newTree.right = mergeTrees(t1.right, t2.right);
+	}
+	if(!t1.left){
+		newTree.left = t2.right;
+	}
+	if (!t2.right) {
+		newTree.right = t2.right
+	}
+
+return newTree;
+};
+
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+// second 
+var mergeTrees = function(t1, t2) {
+	var newTree = new TreeNode(t1.val + t2.val)
+	if(t1.left && t2.left){
+	this.left = mergeTrees(t1.left,t2.left);
+	this.right = mergeTrees(t1.right,t2.right);
+	}
+	else
+	{ 
+
+	}
+	return newTree ;
+}
