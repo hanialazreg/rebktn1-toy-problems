9b02015f5431a6426d4d8788b1ea257e4189dfc5
hanialazreg <lazreghania@gmail.com>
2020-02-05 09:47:56 +0100

    solved 3 ways

--- flattenArray/flatten.js
+++ flattenArray/flatten.js
@@ -7,6 +7,44 @@ Example:
 flatten([[1, 1], [4], 3, [3], 4, [6, 7]]) // [1, 1, 4, 3, 3, 4, 6, 7]
 */
 
+// first solution 
 function flatten(arr) {
-  // your code here...
+	var flat = [];
+  for (var i = 0; i < arr.length; i++) {
+  	if(!Array.isArray(arr[i])){
+  		flat.push(arr[i])
+  	} else 
+  	{
+  		for (var j = 0; j < arr[i].length; j++) {
+  			flat.push(arr[i][j])
+  		}
+  	}
+
+  }
+  return flat
+}
+// with recursion
+function flatten(arr) {
+	var flat = []
+
+	var fl=function(elem){
+		if (Array.isArray(elem)) {
+            
+            for (var i = 0; i < elem.length; ++ i) {
+                fl(elem[i]); 
+            }
+        } else {
+            flat.push(elem); 
+        }
+    }
+
+    fl(arr);
+    return flat
+	
+}
+
+// built in function
+function flatten(arr){
+	var flat = arr.flat();
+	return flat
 }
