7edaaeb24bf575c56a28e224616913a5f7249990
hanialazreg <lazreghania@gmail.com>
2020-02-20 09:52:30 +0100

    ok roman

--- RomanNumeralTranslator/RomanNumeralTranslator.js
+++ RomanNumeralTranslator/RomanNumeralTranslator.js
@@ -14,17 +14,38 @@
 
 // You've got Helpers!
 
-// var DIGIT_VALUES = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-//     L: 50, 
-//     C: 100,
-//     D: 500,
-//     M: 1000
-//   };
+var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
 
-function translateRomanNumeral (romanNumeral) {
-    // Write your code here, and
-    // return your final answer.
+function translateRomanNumeral(romanNumeral) {
+  if (typeof romanNumeral !== "string") {
+    return "null";
   }
+  if (romanNumeral.length === 0) {
+    return 0;
+  }
+
+  var startarr = romanNumeral.split("");
+  var result = 0;
+  for (var i = 0; i < startarr.length; i++) {
+    if (DIGIT_VALUES.hasOwnProperty(startarr[i])) {
+      var current = startarr[i];
+      var next = startarr[i + 1];
+      if (next !== undefined && DIGIT_VALUES[current] > DIGIT_VALUES[next]) {
+        var curvalue = DIGIT_VALUES[current] - DIGIT_VALUES[next];
+        result += curvalue;
+      } else if (DIGIT_VALUES[current] < DIGIT_VALUES[next]) {
+        var curvalue = DIGIT_VALUES[next] + DIGIT_VALUES[current];
+        result += curvalue;
+      }
+    }
+  }
+  return result;
+}
--- wordsWithinWords/wordsWithinWords.js
+++ wordsWithinWords/wordsWithinWords.js
@@ -9,4 +9,30 @@ nestedWordCount([ "gray", "grays", "ray", "rays", "strays" ]) // 'grays'
 
 function nestedWordCount(words) {
   // your code here...
+  let wordsObj = {}
+  let numberOfWords = 0
+  let max = 0
+  let name;
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (words[i].includes(words[j])) {
+        numberOfWords++
+      }
+
+    }
+    wordsObj[words[i]] = numberOfWords
+    numberOfWords = 0
+  }
+
+  for (const key in wordsObj) {
+    if (wordsObj[key] > max) {
+      max = wordsObj[key]
+      name = key
+    }
+    // console.log(wordsObj[key], max)
+  }
+  return name
 }
+console.log(nestedWordCount(["gray", "grays", "ray", "rays", "strays"]))
+
