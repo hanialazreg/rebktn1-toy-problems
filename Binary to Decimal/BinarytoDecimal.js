e01e6e8f2562ecd87bcfcb1e2e7074bd95512d08
hanialazreg <lazreghania@gmail.com>
2020-02-22 09:30:27 +0100

    solved

--- Binary to Decimal/BinarytoDecimal.js
+++ Binary to Decimal/BinarytoDecimal.js
@@ -1,4 +1,4 @@
-// Given a number n (represented as a binary number string), convert n to base 10. Do NOT use parseInt(n, 2). 
+// Given a number n (represented as a binary number string), convert n to base 10. Do NOT use parseInt(n, 2).
 // You may use the parseInt function that takes in just one argument.
 
 // Examples
@@ -11,10 +11,25 @@
 // n:"101"	 5
 // n:"110"	 6
 // n:"111"	 7
-// n:"1000"	 8 
+// n:"1000"	 8
+var objB = {
+  A: 10,
+  B: 11,
+  C: 12,
+  D: 13,
+  E: 14,
+  F: 15
+};
 
-
-
-function binaryToDecimal (n) {
-// code gose here 
+function binaryToDecimal(n) {
+  var res = 0;
+  for (var i = 0; i < n.length; i++) {
+    res += 2 ** (n.length - i - 1) * n[i];
+  }
+  return res;
 }
+
+const binaryToDecimalRec = (n, res = 0) =>
+  n.length
+    ? binaryToDecimalRec(n.slice(1), res + 2 ** (n.length - 1) * n[0])
+    : res;
